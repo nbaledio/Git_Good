c7ff791bfe43b6819be34b3f2152e4d156b4ed47
nbaledio <nbaledio@ucsc.edu>
2019-05-07 18:00:04 -0700

    Fixed Merge Conflict

--- Code/main.js
+++ Code/main.js
@@ -1,6 +1,8 @@
 // be strict
 'use strict';
 
+var game = new Phaser.Game(1600,900, Phaser.AUTO, 'phaser');
+
 // add states to StateManager and start MainMenu
 game.state.add('GameTitle', GameTitle)
 game.state.add('GameTutorial', GameTutorial)
