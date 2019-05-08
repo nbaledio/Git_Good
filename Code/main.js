290b003c35763682e46b66b280dcccee52c43644
nbaledio <nbaledio@ucsc.edu>
2019-05-07 19:07:39 -0700

    Deleted Window Size

--- Code/main.js
+++ Code/main.js
@@ -1,8 +1,6 @@
 // be strict
 'use strict';
 
-var game = new Phaser.Game(1600,900, Phaser.AUTO, 'phaser');
-
 // add states to StateManager and start MainMenu
 game.state.add('GameTitle', GameTitle)
 game.state.add('GameTutorial', GameTutorial)
